290d41be5d71ec7d1d03e455fa395c436bb42a48
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-05-29 17:49:21 -0700

    add liquididty first test passing

--- test/AMM.js
+++ test/AMM.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { transform } = require("lodash");
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
@@ -8,15 +9,21 @@ const tokens = (n) => {
 const ether = tokens;
 
 describe("AMM", () => {
-  let amm, accounts, deployer, token1, token2;
+  let amm, accounts, deployer, token1, token2, liquidityProvider 
 
   beforeEach(async () => {
     accounts = await ethers.getSigners();
     deployer = accounts[0];
+    liquidityProvider = accounts[1]
 
     const Token = await ethers.getContractFactory("Token");
     token1 = await Token.deploy("Dapp University", "DAPP", "1000000");
     token2 = await Token.deploy("USD Token", "USD", "1000000");
+
+    let transaction = await token1.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+    transaction = await token2.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
 
     const AMM = await ethers.getContractFactory("AMM");
     amm = await AMM.deploy(token1.address, token2.address);
@@ -31,6 +38,20 @@ describe("AMM", () => {
     });
     it("returns token2", async () => {
       expect(await amm.token2()).to.equal(token2.address);
+    });
+  });
+  describe("Swapping Tokens", () => {
+    let amount
+    it("facilitates swaps", async () => {
+        amount = tokens(100000)
+        transaction = await token1.connect(deployer).approve(amm.address, amount)
+        await transaction.wait()
+        transaction = await token2.connect(deployer).approve(amm.address, amount)
+        await transaction.wait()
+        transaction = await amm.connect(deployer).addLiquidity(amount, amount)
+        await transaction.wait()
+      expect(await token1.balanceOf(amm.address)).to.equal(amount)
+      expect(await token2.balanceOf(amm.address)).to.equal(amount)
     });
     it("", async () => {});
     it("", async () => {});
