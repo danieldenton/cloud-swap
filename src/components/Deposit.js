9219155530ef1cd1a34e581da8442672628a973c
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-06-07 16:52:40 -0700

    Withdraw component set up with calculate deposit functions reading

--- src/components/Deposit.js
+++ src/components/Deposit.js
@@ -1,5 +1,122 @@
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ethers } from "ethers";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Spinner from "react-bootstrap/Spinner";
+
+import { swap, loadBalances } from "../store/interactions";
+
+import Alert from "./Alert";
 export const Deposit = () => {
-  return <div>Deposit</div>;
+  const [token1Amount, setToken1Amount] = useState(0);
+  const [token2Amount, setToken2Amount] = useState(0);
+
+  const provider = useSelector((state) => state.provider.connection);
+  const account = useSelector((state) => state.provider.account);
+  const tokens = useSelector((state) => state.tokens.contracts);
+  const symbols = useSelector((state) => state.tokens.symbols);
+  const balances = useSelector((state) => state.tokens.balances);
+  const amm = useSelector((state) => state.amm.contract);
+
+  const handleAmount = async (e) => {
+    if (e.target.id === "token1") {
+      setToken1Amount(e.target.value);
+      const _token1Amount = ethers.utils.parseUnits(e.target.value, "ether");
+      const result = await amm.calculateToken2Deposit(_token1Amount);
+      const _token2Amount = ethers.utils.formatUnits(
+        result.toString(),
+        "ether"
+      );
+      setToken2Amount(_token2Amount);
+    } else {
+      setToken2Amount(e.target.value);
+      const _token2Amount = ethers.utils.parseUnits(e.target.value, "ether");
+      const result = await amm.calculateToken1Deposit(_token2Amount);
+      const _token1Amount = ethers.utils.formatUnits(
+        result.toString(),
+        "ether"
+      );
+      setToken1Amount(_token1Amount);
+    }
+  };
+
+  const handleDeposit = async (e) => {
+    e.preventDefault();
+    console.log("handleDeposit");
+  };
+
+  return (
+    <div>
+      <Card style={{ maxWidth: "450px" }} className="mx-auto px-4">
+        {account ? (
+          <Form
+            onSubmit={handleDeposit}
+            style={{ maxWidth: "450px", margin: "50px auto" }}
+          >
+            <Row className="my-3">
+              <Form.Text className="text-end my-2" muted>
+                Balance: {balances[0]}
+              </Form.Text>
+              <InputGroup>
+                <Form.Control
+                  type="number"
+                  placeholder="0.0"
+                  min="0.0"
+                  step="any"
+                  id="token1"
+                  onChange={handleAmount}
+                  value={token1Amount === 0 ? "" : token1Amount}
+                />
+                <InputGroup.Text
+                  style={{ width: "100px" }}
+                  className="justify-content-center"
+                >
+                  {symbols && symbols[0]}
+                </InputGroup.Text>
+              </InputGroup>
+            </Row>
+            <Row className="my-3">
+              <Form.Text className="text-end my-2" muted>
+                Balance: {balances[1]}
+              </Form.Text>
+              <InputGroup>
+                <Form.Control
+                  type="number"
+                  placeholder="0.0"
+                  step="any"
+                  id="token2"
+                  onChange={handleAmount}
+                  value={token2Amount === 0 ? "" : token2Amount}
+                />
+                <InputGroup.Text
+                  style={{ width: "100px" }}
+                  className="justify-content-center"
+                >
+                  {symbols && symbols[1]}
+                </InputGroup.Text>
+              </InputGroup>
+            </Row>
+            <Row>
+              <Button type="submit">Deposit</Button>
+            </Row>
+          </Form>
+        ) : (
+          <p
+            className="d-flex justify-content-center align-items-center"
+            style={{ height: "300px" }}
+          >
+            Please connect wallet
+          </p>
+        )}
+      </Card>
+    </div>
+  );
 };
 
 export default Deposit;
