f916c1d3caa000ba11c49a73e63c3c0b0287d58f
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-06-10 16:56:00 -0700

    formatting functions created

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -12,6 +12,7 @@ export const Charts = () => {
   const tokens = useSelector((state) => state.tokens.contracts);
   const symbols = useSelector((state) => state.tokens.symbols);
   const amm = useSelector((state) => state.amm.contract);
+  const swaps = useSelector((state) => state.amm.swaps);
 
   useEffect(() => {
     if (provider && amm) {
@@ -19,11 +20,66 @@ export const Charts = () => {
     }
   }, [provider, amm, dispatch]);
 
+  const formatHash = (hash) => {
+    const formattedString = hash.slice(0, 5) + "..." + hash.slice(61, 66);
+    return formattedString;
+  };
+
+  const formatAddress = (address) => {
+    const formattedAddy = address.slice(0, 5) + "..." + address.slice(38, 42);
+    return formattedAddy;
+  };
+
+  const getSymbol = (token) => {
+    if (token === tokens[0].address) {
+      return symbols[0];
+    } else if (token === tokens[1].address) {
+      return symbols[1];
+    } else {
+      return "";
+    }
+  };
+
+  const formatEther = (amount) => {
+    const formattedEther = ethers.utils.formatUnits(amount.toString(), "ether");
+    return formattedEther;
+  };
+
+  const formatDate = (date) => {
+    const formattedDate = new Date(
+      Number(date.toString() + "000")
+    ).toLocaleDateString(undefined, {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    });
+    return formattedDate;
+  };
+
+  const tabledSwaps =
+    swaps &&
+    swaps.map((swap, idx) => {
+      return (
+        <tr key={idx}>
+          <td>{formatHash(swap.hash)}</td>
+          <td>{getSymbol(swap.args.tokenGive)}</td>
+          <td>{formatEther(swap.args.tokenGiveAmount)}</td>
+          <td>{getSymbol(swap.args.tokenGet)}</td>
+          <td>{formatEther(swap.args.tokenGetAmount)}</td>
+          <td>{formatAddress(swap.args.user)}</td>
+          <td>{formatDate(swap.args.timestamp)}</td>
+        </tr>
+      );
+    });
+
   return (
     <Table striped bordered hover>
       <thead>
         <tr>
-          <th>Transaction Hash</th>
+          <th>Transaction string</th>
           <th>Token Give</th>
           <th>Amount Give</th>
           <th>Token Get</th>
@@ -32,6 +88,7 @@ export const Charts = () => {
           <th>Time</th>
         </tr>
       </thead>
+      <tbody>{tabledSwaps}</tbody>
     </Table>
   );
 };
