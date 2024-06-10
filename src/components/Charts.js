6e76cc765f214c2742cb1beaca04e1f32f15d8b0
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-06-10 15:59:32 -0700

    loadAllSwaps created as well as swapsLoaded

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,6 +1,25 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ethers } from "ethers";
+import Loading from "./Loading";
+import { loadAllSwaps } from "../store/interactions";
+
 export const Charts = () => {
+  const dispatch = useDispatch();
 
-    return <div>Charts</div>
-}
+  const provider = useSelector((state) => state.provider.connection);
+  const tokens = useSelector((state) => state.tokens.contracts);
+  const symbols = useSelector((state) => state.tokens.symbols);
+  const amm = useSelector((state) => state.amm.contract);
 
-export default Charts
+  useEffect(() => {
+    if (provider && amm) {
+        loadAllSwaps(provider, amm, dispatch);
+    }
+  }, [provider, amm, dispatch]);
+
+  
+  return <div>Charts</div>;
+};
+
+export default Charts;
