d3200266da5319e3fadb5264f9b134570585cd2c
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-06-11 08:16:54 -0700

    data is correct

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,16 +1,42 @@
-import { createSelector } from "reselect"
+import { createSelector } from "reselect";
 
-const tokens = state => state .tokens.contracts 
-const swaps = state => state.amm.swaps
+const tokens = (state) => state.tokens.contracts;
+const swaps = (state) => state.amm.swaps;
 
-export const chartSelector =  createSelector(swaps, tokens, (swaps, tokens) => {
-    console.log(swaps, tokens)
+export const chartSelector = createSelector(swaps, tokens, (swaps, tokens) => {
+  if (!tokens[0] || !tokens[1]) {
+    return;
+  }
 
-    return({
-        series: [{
-            name: "Rate",
-            data: [30, 40, 45, 50, 49, 60, 70, 91]
-        }]
-    })
-})
+  swaps = swaps.filter(
+    (s) =>
+      s.args.tokenGet === tokens[0].address ||
+      s.args.tokenGet === tokens[1].address
+  );
+  swaps = swaps.filter(
+    (s) =>
+      s.args.tokenGive === tokens[0].address ||
+      s.args.tokenGive === tokens[1].address
+  );
 
+  swaps.sort((a, b) => a.args.timestamp - b.args.timestamp);
+
+  const decorateSwap = (swap) => {
+    let rate = swap.args.token2Balance / swap.args.token1Balance;
+    rate = Math.round(rate * 100000) / 100000
+    return { ...swap, rate };
+  };
+
+  swaps = swaps.map((s) => decorateSwap(s));
+
+  const prices = swaps.map(s => s.rate)
+
+  return {
+    series: [
+      {
+        name: "Rate",
+        data: prices,
+      },
+    ],
+  };
+});
