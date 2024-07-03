2ac3f2ae8802b03c40c667bd5f44f00ab3aeb8fc
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-07-02 19:33:46 -0700

    linkPills updaated

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,25 +1,49 @@
 import Nav from "react-bootstrap/Nav";
 import { LinkContainer } from "react-router-bootstrap";
+import { useLocation } from "react-router-dom";
 
 export const Tabs = () => {
+  const location = useLocation();
+
+  const links = [
+    {
+      to: "/",
+      text: "Swap",
+    },
+    {
+      to: "/deposit",
+      text: "Deposit",
+    },
+    {
+      to: "/withdraw",
+      text: "Withdraw",
+    },
+    {
+      to: "/charts",
+      text: "Charts",
+    },
+  ];
+
+  const linkPills = links.map((link, idx) => {
+    return (
+      <LinkContainer
+        to={link.to}
+        className={
+          location.pathname === link.to ? "bg-danger text-dark" : "text-danger"
+        }
+      >
+        <Nav.Link>{link.text}</Nav.Link>
+      </LinkContainer>
+    );
+  });
+
   return (
     <Nav
       variant="pills"
       defaultActiveKey="/"
       className="justify-content-center my-4"
     >
-      <LinkContainer to="/">
-        <Nav.Link>Swap</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/deposit">
-        <Nav.Link>Deposit</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/withdraw">
-        <Nav.Link>Withdraw</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="charts">
-        <Nav.Link>Charts</Nav.Link>
-      </LinkContainer>
+      {linkPills}
     </Nav>
   );
 };
