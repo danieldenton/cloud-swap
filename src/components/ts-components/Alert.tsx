38da11aedea5096cc44a2d174ff3beb4ad44b073
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-07-08 21:41:40 -0700

    typescipt underway

--- src/components/ts-components/Alert.tsx
+++ src/components/ts-components/Alert.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Alert as BootstrapAlert } from "react-bootstrap";
 
-const Alert = ({ message, transactionHash, variant, setShowAlert }) => {
+interface Props {
+  message: string;
+  transactionHash: string | null;
+  variant: string;
+  setShowAlert: (arg0: boolean) => void;
+}
+
+const Alert = ({ message, transactionHash, variant, setShowAlert }: Props) => {
   return (
     <BootstrapAlert
       variant={variant}
---
+++ src/types/state.ts
@@ -0,0 +1,49 @@
+export interface ProviderState {
+    connection: any;
+    chainId: number;
+    account: string;
+  }
+  
+  export interface TokensState {
+    contracts: any;
+    symbols: string[];
+    balances: number[];
+  }
+  
+  interface Swapping {
+    isSwapping: boolean;
+    isSuccess: boolean;
+    transactionHash: string;
+  }
+  
+  interface Withdrawing {
+    isWithdrawing: boolean;
+    isSuccess: boolean;
+    transactionHash: string;
+  }
+  
+  export interface AMMState {
+    contract: any;
+    shares: number;
+    swapping: Swapping;
+    withdrawing: Withdrawing;
+  }
+  
+  interface Addresses {
+    rump: { address: string };
+    usd: { address: string };
+    bloodMoonSwap: { address: string };
+    cloudSwap: { address: string };
+    dexAggregator: { address: string };
+  }
+  
+  export interface Config {
+    [key: number]: Addresses;
+  }
+  
+  export interface RootState {
+    provider: ProviderState;
+    tokens: TokensState;
+    amm: AMMState;
+  }
+  
